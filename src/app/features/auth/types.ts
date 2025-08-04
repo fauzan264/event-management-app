b2384452b7d9adb13bb30d1ffc9cd3958adab85d
Fauzan <fauzan.c4@gmail.com>
2025-08-05 05:15:33 +0700

    feat: setup auth API integration using axios

---
+++ src/app/features/auth/types.ts
@@ -0,0 +1,14 @@
+export interface IAuth {
+  id: string;
+  idCardNumber: string;
+  fullname: string;
+  dateOfBirth: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  referralCode: string;
+  userRole: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+}
---
+++ src/app/services/auth.ts
@@ -0,0 +1,84 @@
+import snakecaseKeys from "snakecase-keys";
+import { IAuth } from "../features/auth/types";
+import axiosInstance from "../lib/axiosInstances";
+
+export const login = ({
+  email,
+  password,
+}: Pick<IAuth, "email" | "password">) => {
+  return axiosInstance.post("/auth/login", { email, password });
+};
+
+export const register = ({
+  idCardNumber,
+  fullname,
+  dateOfBirth,
+  email,
+  password,
+  phoneNumber,
+  referralCode,
+  userRole,
+}: Pick<
+  IAuth,
+  | "idCardNumber"
+  | "fullname"
+  | "dateOfBirth"
+  | "email"
+  | "password"
+  | "phoneNumber"
+  | "referralCode"
+  | "userRole"
+>) => {
+  return axiosInstance.post(
+    "/auth/register",
+    snakecaseKeys({
+      idCardNumber,
+      fullname,
+      dateOfBirth,
+      email,
+      password,
+      phoneNumber,
+      referralCode,
+      userRole,
+    })
+  );
+};
+
+export const requestResetPassword = ({ email }: Pick<IAuth, "email">) => {
+  return axiosInstance.post("/auth/request-reset-password", { email });
+};
+
+export const resetPassword = ({
+  password,
+  token,
+}: Pick<IAuth, "password"> & { token: string }) => {
+  return axiosInstance.post(
+    `/auth/reset-password`,
+    { password },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
+
+export const changePassword = ({
+  oldPassword,
+  newPassword,
+  token,
+}: {
+  oldPassword: string;
+  newPassword: string;
+  token: string;
+}) => {
+  return axiosInstance.post(
+    `/auth/change-password`,
+    snakecaseKeys({ oldPassword, newPassword }),
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
