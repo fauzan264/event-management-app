2dd08d3cee62368fdbe56c00d88277446e4b79bf
Fauzan <fauzan.c4@gmail.com>
2025-08-09 17:46:17 +0700

    wip: delete event page

--- src/services/event.ts
+++ src/services/event.ts
@@ -1,6 +1,121 @@
-import { IEvent } from "@/features/event/types";
+import { IEvent, IVenue } from "@/features/event/types";
 import axiosInstance from "@/lib/axiosInstances";
 
 export const detailEvent = ({ id }: Pick<IEvent, "id">) => {
   return axiosInstance.get(`/events/${id}`);
+};
+
+export const createEvent = ({
+  eventName,
+  category,
+  startDate,
+  endDate,
+  description,
+  availableTicket,
+  price,
+  venueName,
+  venueCapacity,
+  address,
+  image,
+  token,
+}: Pick<
+  IEvent,
+  | "eventName"
+  | "category"
+  | "startDate"
+  | "endDate"
+  | "description"
+  | "availableTicket"
+  | "price"
+> &
+  Pick<IVenue, "venueName" | "venueCapacity" | "address"> & {
+    image: string;
+    token: string;
+  }) => {
+  return axiosInstance.post(
+    "/events",
+    {
+      eventName,
+      category,
+      startDate,
+      endDate,
+      description,
+      availableTicket,
+      price,
+      venueName,
+      venueCapacity,
+      address,
+      image,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
+
+export const updateEvent = ({
+  id,
+  eventName,
+  category,
+  startDate,
+  endDate,
+  description,
+  availableTicket,
+  price,
+  venueName,
+  venueCapacity,
+  address,
+  image,
+  token,
+}: Pick<
+  IEvent,
+  | "id"
+  | "eventName"
+  | "category"
+  | "startDate"
+  | "endDate"
+  | "description"
+  | "availableTicket"
+  | "price"
+> &
+  Pick<IVenue, "venueName" | "venueCapacity" | "address"> & {
+    image: string;
+    token: string;
+  }) => {
+  return axiosInstance.put(
+    `/events/${id}`,
+    {
+      eventName,
+      category,
+      startDate,
+      endDate,
+      description,
+      availableTicket,
+      price,
+      venueName,
+      venueCapacity,
+      address,
+      image,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
+
+export const deleteEvent = ({
+  id,
+  token,
+}: Pick<IEvent, "id"> & {
+  token: string;
+}) => {
+  return axiosInstance.delete(`/events/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
